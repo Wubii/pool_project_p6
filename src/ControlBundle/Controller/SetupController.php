e8266af78f00a62b95084a1a93523fdb5d393df5
Wubiii <battista.marion@gmail.com>
2017-07-12 15:40:22 +0000

    Add setup controller

--- src/ControlBundle/Controller/SetupController.php
+++ src/ControlBundle/Controller/SetupController.php
@@ -4,8 +4,33 @@ namespace ControlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form;
+
+use ControlBundle\Entity\MbSetup;
+
+use ControlBundle\Form\MbSetupType;
 
 class SetupController extends Controller
 {
-	// setup pour les capteurs
+	/**
+     * @Route("/control/setup", name="CONTROL_setup")
+     */
+    public function setupAction(Request $request)
+    {
+		$setup = new MbSetup();
+		
+	    $form = $this->get('form.factory')->create(MbSetupType::class, $setup);
+	    $form->handleRequest($request);
+	           
+        if($form->isValid())
+        {
+    	    
+        }
+	    
+	    // replace this example code with whatever you need
+        return $this->render('control/setup.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
